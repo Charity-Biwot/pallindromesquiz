c74ccd6cfffa4ef565ee4eeb58c74736c690636c
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-06-24 11:08:12 +0300

    first commit

---
+++ pallindrome.py
@@ -0,0 +1,9 @@
+#a program that check if pallindromes are present in a string
+def isPalindrome(s):
+    return s
+s = "eye"
+output = isPalindrome
+if output:
+    print("yes")
+else:
+    print("no")
